67be065f8f6e35a5e074341bbc0efb9180238824
Daan Buit <daan@buit.nu>
2019-09-27 11:15:33 +0200

    Start on media player control

--- src/app/modal/media.modal.page.ts
+++ src/app/modal/media.modal.page.ts
@@ -1,5 +1,5 @@
-import {Component, Input, SimpleChange, SimpleChanges} from '@angular/core';
-import { callService, Connection } from 'home-assistant-js-websocket';
+import {Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {callService, Connection, subscribeEntities} from 'home-assistant-js-websocket';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -7,23 +7,72 @@ import { ModalController } from '@ionic/angular';
     styleUrls: ['media-modal-page.scss'],
     selector: 'media-modal-page',
 })
-export class MediaModalPage  {
+export class MediaModalPage implements OnInit {
 
     @Input() entity: any;
     @Input() entityData: any;
     @Input() connection: any;
+    volume: number;
 
     constructor(public modalController: ModalController) {
     }
 
     ngOnInit() {
+        this.volume = this.entityData.attributes.volume_level * 100;
+        subscribeEntities(this.connection, entities => {
+            this.entityData = entities[this.entity.entity];
+            this.volume = this.entityData.attributes.volume_level * 100;
+        });
+
 
     }
 
-
+    getThumbnail() {
+        // callService(this.connection, "homeassistant", "toggle", {
+        //     entity_id: entity
+        // });
+    }
 
     dismissModal() {
         this.modalController.dismiss();
     }
 
+    //Set favorite as source
+    setSource() {
+        //select_source
+    }
+
+    //Join player
+    joinPlayer() {
+        //sonos //join
+    }
+
+    //Unjoin player
+    unjoinPlayer() {
+        //sonos //unjoin
+    }
+
+    playPauseAction() {
+        if (this.entityData.state === 'playing') {
+            this.pause();
+        } else {
+            this.play();
+        }
+    }
+
+    pause() {
+        callService(this.connection, 'media_player', 'media_pause', {
+            entity_id: this.entity.entity
+        });
+    }
+
+    play() {
+        callService(this.connection, 'media_player', 'media_play', {
+            entity_id: this.entity.entity
+        });
+    }
+
+    setVolume() {
+        //volume_set
+    }
 }
